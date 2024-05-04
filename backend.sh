75352e3691f73a2954b9dd01b7a2527bb965331f
Nanajee ad <nanajee.k9@gmail.com>
2024-05-04 14:29:35 +0530

    add all files

---
+++ backend.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$R $2....FAILURE $N"
+    exit 1
+    else
+    echo -e "$G $2....SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access"
+    exit 1
+else
+    echo -e "$R you are super user $N"
+fi
+
+dnf module disable nodejs -y &>>LOGFILE
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>LOGFILE
+VALIDATE $? "Enabling nodejs:20 version"
+
+dnf install nodejs -y &>>LOGFILE
+VALIDATE $? "Installing nodejs"
+
+useradd expense
+VALIDATE $? "Creating expense user"
+
+
+
+
+
